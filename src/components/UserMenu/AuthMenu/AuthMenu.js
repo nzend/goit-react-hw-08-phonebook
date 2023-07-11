bb434bfe66b144b3d644f18e83f45b821db54cc0
nzend <115723110+nzend@users.noreply.github.com>
2023-07-11 20:46:46 +0300

    Update AuthMenu.js

--- src/components/UserMenu/AuthMenu/AuthMenu.js
+++ src/components/UserMenu/AuthMenu/AuthMenu.js
@@ -1,14 +1,14 @@
 import css from './AuthMenu.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import authSelectors from '../../../redux/auth/auth-selectors';
+// import { useDispatch, useSelector } from 'react-redux';
+// import authSelectors from '../../../redux/auth/auth-selectors';
 import { NavLink } from 'react-router-dom';
 
 const AuthMenu = () => {
   let activeClassName = {
     color: '#f87719',
   };
-  const dispatch = useDispatch();
-  const name = useSelector(authSelectors.getUserName);
+  // const dispatch = useDispatch();
+  // const name = useSelector(authSelectors.getUserName);
   return (
     <ul className={css.user__bar}>
       <li className={css.navigation__item}>
